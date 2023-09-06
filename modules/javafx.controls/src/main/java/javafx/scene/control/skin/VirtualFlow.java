9eed8e483d243cc03d6bf8ec697326e6e0b40154
Jose Pereda <jpereda@openjdk.org>
2023-09-06 11:21:00 +0000

    8310638: Filtering a TableView with a large number of items freezes the UI
    Reviewed-by: jvos
    Backport-of: 8b1a446ca9519f468aa1cf6ee9876b6035dcac37

--- modules/javafx.controls/src/main/java/javafx/scene/control/skin/VirtualFlow.java
+++ modules/javafx.controls/src/main/java/javafx/scene/control/skin/VirtualFlow.java
@@ -859,6 +859,9 @@ public class VirtualFlow<T extends IndexedCell> extends Region {
             int oldIndex = computeCurrentIndex(oldCount);
             double oldOffset = computeViewportOffset(getPosition(), oldCount);
             int cellCount = get();
+            if (oldIndex > cellCount) {
+                oldIndex = cellCount;
+            }
             resetSizeEstimates();
             recalculateAndImproveEstimatedSize(DEFAULT_IMPROVEMENT, oldIndex, oldOffset);
 
@@ -3005,7 +3008,11 @@ public class VirtualFlow<T extends IndexedCell> extends Region {
         return baseOffset;
     }
 
-    private int computeCurrentIndex() {
+    /**
+     * Compute the index of the first visible cell
+     * This has package access ONLY FOR TESTING.
+     */
+    int computeCurrentIndex() {
         return computeCurrentIndex(getCellCount());
     }
 
